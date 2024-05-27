68d6273c993e82bcb1fa6abf01bbbc713621a4ce
cpadubidri <chirag.padubidri@gmail.com>
2024-05-27 16:36:21 +0300

    Update batch gen to accomodate folder structure

--- georef_batch.py
+++ georef_batch.py
@@ -3,15 +3,18 @@ import os
 from tqdm import tqdm
 
 if __name__=="__main__":
-    folder_path = input("Enter the folder path")
+    folder_path = input("Enter the folder path : ")
     savefolder = None
     start = stop = None
-    count = 0
 
     acq_list = os.listdir(folder_path)
+    count = len(acq_list)
     for acq in acq_list:
         print(f"Processing Acqisition {acq} and remaining is {count}")
         imagePath = os.path.join(folder_path,acq)
         print(imagePath)
-        main(imagePath=imagePath, saveFolder=savefolder, start=start, stop=stop)
-        count+=1
+        main(imagePath=imagePath,vrt=True, saveFolder=savefolder, start=start, stop=stop)
+        count-=1
+
+
+#/home/savvas/SUPER-NAS/USERS/Chirag/PERIOPSIS/202405-Greece/Data/Larissa/larissa

a18a526de34655c1c9bf6ea088a811a5e10af441
cpadubidri <chirag.padubidri@gmail.com>
2024-05-28 18:37:17 +0300

    Remove GUI feature

--- georef_batch.py
+++ georef_batch.py
@@ -1,18 +1,9 @@
 from georef import main
 import os
 from tqdm import tqdm
-from guiHandler import getFolder
 
-def batchGeoref():
-    try:
-        folder_path = getFolder()
-        if not folder_path:
-            raise Exception("No folder selected")
-    except Exception as e:
-        print(f"GUI failed or no folder selected ({e}). Please enter the folder path manually:")
-        folder_path = input("Enter the folder path: ")
-
-    savefolder = None
+def batchGeoref(folder_path, savepath=None):
+    savefolder = savepath
     start = stop = None
 
     acq_list = os.listdir(folder_path)
@@ -20,11 +11,12 @@ def batchGeoref():
     for acq in acq_list:
         print(f"Processing Acqisition {acq} and remaining is {count}")
         imagePath = os.path.join(folder_path,acq)
-        main(imagePath=imagePath, vrt=True, saveFolder=savefolder, start=start, stop=stop)
+        main(imagePath=imagePath, vrt=True, saveFolder=os.path.join(savefolder, acq+'_GEOTAGGED'), start=start, stop=stop)
         count-=1
 
 if __name__=="__main__":
-    batchGeoref()
+    folder_path = input("Enter the folder path: ")
+    batchGeoref(folder_path, savepath='/home/savvas/StorageNVME2/Greece-Dumping/Trikala')
 
 
 #/home/savvas/SUPER-NAS/USERS/Chirag/PERIOPSIS/202405-Greece/Data/Larissa/larissa

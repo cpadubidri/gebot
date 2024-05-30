0ea9fb4bfb4d30e97055d657b8e77187accb0b67
cpadubidri <chirag.padubidri@gmail.com>
2024-05-30 13:28:13 +0300

    Add enter path feature

--- georef_batch.py
+++ georef_batch.py
@@ -4,19 +4,21 @@ from tqdm import tqdm
 
 def batchGeoref(folder_path, savepath=None):
     savefolder = savepath
-    start = stop = None
+    start = None
+    stop = 5
 
     acq_list = os.listdir(folder_path)
     count = len(acq_list)
-    for acq in acq_list:
+    for acq in acq_list[:1]:
         print(f"Processing Acqisition {acq} and remaining is {count}")
         imagePath = os.path.join(folder_path,acq)
         main(imagePath=imagePath, vrt=True, saveFolder=os.path.join(savefolder, acq+'_GEOTAGGED'), start=start, stop=stop)
         count-=1
 
 if __name__=="__main__":
-    folder_path = input("Enter the folder path: ")
-    batchGeoref(folder_path, savepath='/home/savvas/StorageNVME2/Greece-Dumping/Trikala')
+    folder_path = '/home/savvas/SUPER-NAS/USERS/Chirag/PERIOPSIS/202405-Greece/Data/Trikala/trikala'
 
+    if folder_path==None:
+        folder_path = input("Enter the folder path: ")
+    batchGeoref(folder_path, savepath=folder_path)
 
-#/home/savvas/SUPER-NAS/USERS/Chirag/PERIOPSIS/202405-Greece/Data/Larissa/larissa
